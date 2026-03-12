d63b18a999f4ee779acdf32d9e107158740feab9
Rodrigo Frutuoso <rodrigoafonsofrutuoso@gmail.com>
2026-03-12 18:35:37 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 javac -d out src/sperta/server/SpertaServer.java src/sperta/client/SpertaClient.java
 
 $size = (Get-Item .\out\SpertaClient.class).Length
-"SpertaClient:$size" | Set-Content .\out\attestation.txt
+"SpertaClient:$size" | Set-Content .\src\sperta\server\attestation.txt
 
 Write-Host "Compilado. attestation.txt atualizado: SpertaClient:$size"
 #sempre que compilamos, o size muda
